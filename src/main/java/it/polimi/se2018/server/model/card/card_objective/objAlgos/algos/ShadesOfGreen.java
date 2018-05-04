63c385ab4538c41053a2886520864a9afdf63fe7
simolanzillotta <simone.lanzillotta@mail.polimi.it>
2018-05-05 00:56:41 +0200

    Renamed Form Algorithm13.java.

---
+++ src/main/java/it/polimi/se2018/server/model/card/card_objective/objAlgos/algos/ShadesOfGreen.java
@@ -0,0 +1,28 @@
+package it.polimi.se2018.server.model.card.card_objective.objAlgos.algos;
+
+import it.polimi.se2018.server.model.Player;
+import it.polimi.se2018.server.model.card.card_objective.objAlgos.StrategyAlgorithm;
+
+/*
+    Algoritm13:
+    Nome: "Sfumature Verdi - Privata"
+    Effetto: "Somma dei valori su tutti i dadi verdi"
+    Punti Favore: #
+*/
+
+public class ShadesOfGreen implements StrategyAlgorithm{
+    @Override
+    public int use(Player player) throws Exception {
+        int favours =0;
+        String tempColor;
+        int tempNumber;
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<5; j++) {
+                tempColor = player.getDiceColor(i,j);
+                tempNumber = player.getDiceNumber(i,j);
+                if(tempColor=="Verdi") favours = favours+tempNumber;
+            }
+        }
+        return favours;
+    }
+}
