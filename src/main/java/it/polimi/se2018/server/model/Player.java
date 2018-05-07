9749b30e3b546fe5047ee83b9b42f67ceae529c2
simolanzillotta <simone.lanzillotta@mail.polimi.it>
2018-05-08 00:25:47 +0200

    Aggiornato nomi attributi e inserito un commento su un eventuale modifica

--- src/main/java/it/polimi/se2018/server/model/Player.java
+++ src/main/java/it/polimi/se2018/server/model/Player.java
@@ -2,56 +2,60 @@ package it.polimi.se2018.server.model;
 
 import it.polimi.se2018.server.exceptions.InvalidCellException;
 import it.polimi.se2018.server.exceptions.invalid_value_exceptios.InvalidCoordinatesException;
+import it.polimi.se2018.server.exceptions.invalid_value_exceptios.InvalidFavoursValueException;
+import it.polimi.se2018.server.exceptions.invalid_value_exceptios.InvalidShadeValueException;
 import it.polimi.se2018.server.model.card.card_objective.Objective;
 import it.polimi.se2018.server.model.card.card_schema.Side;
 import it.polimi.se2018.server.model.dice_sachet.Dice;
 
-
-/*
-    Ho provissoriamente inserito nella calsse player dei metodi che mi permettano di estrarre le informazioni sia
-    sulle celle del Side delgiocatore sia sugli eventuali dadi che vi sono posizionati.
-
-    L'ho fatto per poter scrivere gli algoritmi 1,2, ma essendo che si tartta di scelte implementative comuni, ho
-    solo scritto due algoritmi che ne fanno uso. Gli altri dovrò completarli non appena definiamo bene cosa va
-    dentro Player
-
-*/
-
 public class Player {
+
+    /*
+        OVERVIEW: la classe Player è una rappresentazione nel model del client (giocatore) partecipante alla sessione di gioco, identificato da un NOME,
+        un SIDE, un OBJECTIVE e dai segnalini FAVORE.
+
+        Si stava pensando a introdurre una modifica nel costruttore di player. Cosi come lo si è pensato, stiamo ipotizzando che chi chiama il costruttore
+        di Player (si presume il controller ma anche lo stesso Table andrebbe bene) sappia già a priori quale sia la carta Schema da assegnarli. Quindi si
+        dovrebbero creare strutture dati in cui memorizzare temporaneamente le informazioni relative al giocatore, attendere la scelta della carte e solo
+        in seguito chiamare il costrutture.
+
+        Sarebbe invece più corretto chiamare il costruttore di Player per settare gli attributi NAME, MYOBJECTIVE e FAVOURS, e solo in secondo luogo
+        tramite magari un secondo costruttore in override, settare il campo MYSIDE con la scelta effettuata.
+
+    */
+
     private String name;
-    private Side choicePattern;
-    private Objective obj;
+    private Side mySide;
+    private Objective myobjective;
     private int favours;
     private boolean isMyTurn;
 
-    public Player(Side choicePattern, Objective obj, int favours, String nomine) {
-        this.choicePattern = choicePattern;
-        this.obj = obj;
+    public Player(Side choicePattern, Objective objective, int favours, String nomine) {
+        this.mySide = choicePattern;
+        this.myobjective = objective;
         this.favours = favours;
         this.name=nomine;
     }
 
     public String getColorCell(int i, int j) throws Exception{
-        return choicePattern.getColor(i,j);
+        return mySide.getColor(i,j);
     }
 
     public int getNumberCell(int i, int j)  throws Exception{
-        return choicePattern.getNumber(i,j);
+        return mySide.getNumber(i,j);
     }
-
 
     //Ritorna il colore del dado posizionato nella cella (i,j), se esiste
     public String getDiceColor(int i, int j) throws Exception{
-        return choicePattern.getCellsDiceColorInformation(i,j);
+        return mySide.getCellsDiceColorInformation(i,j);
     }
 
     //Ritorna il numero del dado posizionato nella cella (i,j), se esiste
     public int getDiceNumber(int i, int j) throws Exception{
-        return choicePattern.getCellsDiceNumberInformation(i,j);
+        return mySide.getCellsDiceNumberInformation(i,j);
     }
 
     public void putDice(Dice d, int row, int col) throws InvalidCellException, InvalidCoordinatesException{
-        this.choicePattern.put(row,col,d);
+        this.mySide.put(row,col,d);
     }
-
 }
