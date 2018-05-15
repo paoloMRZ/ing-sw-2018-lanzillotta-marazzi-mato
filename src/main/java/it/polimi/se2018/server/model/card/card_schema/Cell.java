54a69dc1c661592c84730c7c6820d34e41114665
paoloMRZ <paolo3.marazzi@mail.polimi.it>
2018-05-15 11:07:29 +0200

    Ho agginuto i commenti di javaDoc alla classe.

--- src/main/java/it/polimi/se2018/server/model/card/card_schema/Cell.java
+++ src/main/java/it/polimi/se2018/server/model/card/card_schema/Cell.java
@@ -7,18 +7,32 @@ import it.polimi.se2018.server.exceptions.invalid_value_exceptios.InvalidColorVa
 import it.polimi.se2018.server.exceptions.invalid_value_exceptios.InvalidShadeValueException;
 import it.polimi.se2018.server.model.dice_sachet.Dice;
 
-public class Cell {
 
-    //OVERVIEW: La classe rappresenta una singola cella di una carta schema con le sue possibili restrizioni e
-    //l'eventuale dado posizionato su di essa.
-    //Si noti che una cella non può cambiare le sue restrizioni durate la partita, quindi queste vengono dichiarate alla
-    //nascita della cella e non vengono mai modificate.
+/**
+ * La classe rappresenta una singola cella di una carta schema con le sue possibili restrizioni e
+ * l'eventuale dado posizionato su di essa.
+ * Si noti che una cella non può cambiare le sue restrizioni durate la partita, quindi queste vengono dichiarate alla
+ * nascita della cella e non vengono mai modificate.
+ *
+ * @author Marazzi Paolo
+ */
+
+public class Cell {
 
     private Dice dice; //Eventuale dado posizionato sulla cella.
     private String color; //Eventuale restrizione sul colore.
     private int number; //Eventuale restrizione sulla sfumatura.
 
-    //Costruttore.
+
+    /**
+     * Costruttore della classe. Crea una cella che non contiene nessun dado.
+     *
+     * @param color restrizione di colore della cella.
+     * @param number restrizione di sfumatura della cella.
+     * @throws InvalidShadeValueException viene lanciata se viene passato una restrizione di sfumatura non valida.
+     * @throws InvalidColorValueException viene lanciata se viene passato una restrizione di colore non valida.
+     */
+
     public Cell(String color, int number) throws InvalidShadeValueException, InvalidColorValueException {
 
         if(number>= 0 && number<=6)
@@ -34,7 +48,15 @@ public class Cell {
         this.dice = null;
     }
 
-    //Costruttore che prende in ingresso anche un dice
+    /**
+     * Costruttore della classe. Crea una cella contenente una dado.
+     *
+     * @param color restrizione di colore della cella.
+     * @param number restrizione di sfumatura della cella.
+     * @param d dado contenuto nella cella.
+     * @throws InvalidShadeValueException viene lanciata se viene passato una restrizione di sfumatura non valida.
+     * @throws InvalidColorValueException viene lanciata se viene passato una restrizione di colore non valida.
+     */
     public Cell(String color, int number, Dice d) throws InvalidShadeValueException, InvalidColorValueException {
 
         if(number>= 0 && number<=6)
@@ -53,24 +75,17 @@ public class Cell {
             throw new NullPointerException();
     }
 
-    public String getColor() {
-        return color;
-    }
 
-    public int getNumber() {
-        return number;
-    }
 
-    //Il metodo preleva il dado dalla cella e lo restituisce (se non c'è nessun dado restituisce null). Questo significa
-    //che la cella perde il riferimento al dado.
-
-    public Dice pickDice(){
-        Dice tmp = this.dice;
-        this.dice = null;
-        return  tmp;
-    }
-
-    //Il metodo inserisce un dado alla cella controllando le restrizioni di colore e sfumatura.
+    /**
+     * Il metodo inserisce un dado alla cella controllando le restrizioni di colore e sfumatura.
+     *
+     * @param d dado da inserire nella cella.
+     *
+     * @throws NotEmptyCellException lanciata quando una cella è già occupata da un dado.
+     * @throws InvalidColorException lanciata quando il dado da inserire non rispetta la restrizione di colore della cella.
+     * @throws InvalidShadeException lanciata quando il dado da inserire non rispetta la restrizione di sfumatura della cella.
+     */
     public void putDice(Dice d) throws NotEmptyCellException, InvalidColorException, InvalidShadeException {
 
         //Controlla se la cella è già occupata, se si lancia un'eccezione.
@@ -88,8 +103,14 @@ public class Cell {
         this.dice = new Dice(d.getColor(), d.getNumber());
     }
 
-
-    //Il metodo aggiunge un dado alla cella ignorando le restrizini di colore.
+    /**
+     * Il metodo aggiunge un dado alla cella ignorando le restrizini di colore.
+     *
+     * @param d dado da inserire nella cella.
+     *
+     * @throws NotEmptyCellException lanciata quando una cella è già occupata da un dado.
+     * @throws InvalidShadeException lanciata quando il dado da inserire non rispetta la restrizione di sfumatura della cella.
+     */
     public void putDiceIgnoringColor(Dice d) throws NotEmptyCellException, InvalidShadeException {
 
         //Controlla se la cella è già occupata, se si lancia un'eccezione.
@@ -104,6 +125,14 @@ public class Cell {
 
     }
 
+    /**
+     * Il metodo aggiunge un dado alla cella ignorando le restrizini di sfumatura.
+     *
+     * @param d dado da inserire nella cella.
+     *
+     * @throws NotEmptyCellException lanciata quando una cella è già occupata da un dado.
+     * @throws InvalidColorException lanciata quando il dado da inserire non rispetta la restrizione di colore della cella.
+     */
     public void putDiceIgnoringShade(Dice d) throws NotEmptyCellException, InvalidColorException {
 
         //Controlla se la cella è già occupata, se si lancia un'eccezione.
@@ -117,8 +146,24 @@ public class Cell {
         this.dice = new Dice(d.getColor(), d.getNumber());
     }
 
-    //Il metodo restituisce ( ma non rimuove ) l'eventuale dado contenuto nella cella. Se nessun dado è presente
-    //restituisce null.
+    /**
+     * Il metodo preleva il dado dalla cella e lo restituisce (se non c'è nessun dado restituisce null). Questo significa
+     * che la cella perde il riferimento al dado.
+     * Se nessun dado é contenuto restituisce null.
+     *
+     * @return Dice dado (eventualmente) contenuto nella cella.
+     */
+    public Dice pickDice(){
+        Dice tmp = this.dice;
+        this.dice = null;
+        return  tmp;
+    }
+
+    /** Il metodo restituisce ( ma non rimuove ) l'eventuale dado contenuto nella cella.
+     * Se nessun dado è presente restituisce null.
+     *
+     * @return Dice dado (eventualmente) contenuto nella cella.
+     */
     public Dice showDice(){
         if(this.dice != null)
             return new Dice(this.dice.getColor(), this.dice.getNumber()); //Faccio in questo modo per non esporre l'oggetto privato.
@@ -127,11 +172,44 @@ public class Cell {
     }
 
 
+    /**
+     *Il metodo restituisce la restrizione di colore della cella.
+     *
+     * @return String.
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Il metodo restituisce la restrizione di sfumatura della cella.
+     *
+     * @return int
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Restituisce il colore del dado contenuto nella cella.
+     * Se nessun dado è presente restituisce null.
+     *
+     * @deprecated
+     * @return String colore.
+     */
     public String getCellsDiceColor(){
         if(this.dice != null) return dice.getColor();
         else return null;
     }
 
+
+    /**
+     * Restituisce il numero del dado contenuto nella cella.
+     * Se nessun dado è presente restituisce null.
+     *
+     * @deprecated
+     * @return int valore.
+     */
     public int getCellsDiceNumber(){
         if(this.dice != null) return dice.getNumber();
         else return 0;
