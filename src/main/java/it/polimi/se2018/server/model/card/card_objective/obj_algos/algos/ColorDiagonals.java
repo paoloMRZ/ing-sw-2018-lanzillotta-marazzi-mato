0623384d51ef4c886fdf1beafb426c61e5855388
simolanzillotta <simone.lanzillotta@mail.polimi.it>
2018-05-21 09:31:48 +0200

    Aggiunta documentazione in JavaDoc

--- src/main/java/it/polimi/se2018/server/model/card/card_objective/obj_algos/algos/ColorDiagonals.java
+++ src/main/java/it/polimi/se2018/server/model/card/card_objective/obj_algos/algos/ColorDiagonals.java
@@ -9,23 +9,48 @@ import it.polimi.se2018.server.model.card.card_objective.obj_algos.StrategyAlgor
 import it.polimi.se2018.server.model.dice_sachet.Dice;
 
 
-/*
-    Algoritmo9:
-    Nome: "Diagonali Colorate"
-    Effetto: "Numero di dadi dello stesso colore diagonalmente adiacenti"
-    Punti Favore: #
-*/
+/**
+ * Rappresenta l'incapsulamento dell'algoritmo relativo alla carta "Diagonali Colorate" : "Numero di dadi dello stesso colore diagonalmente adiacenti"
+ * con Punti Favore #
+ *
+ * I metodi contrassegnati con "last" nel loro nome sono utilizzati per analizzare le celle dell'ultima riga della carta Side o eventualemente le Celle
+ * che non hanno prossime adiacenze (quindi le ultime possibili "vicine" di celle precedenti
+ *
+ * @author Simone Lanzillotta
+ */
+
 
 public class ColorDiagonals implements StrategyAlgorithm {
 
-    //Metodo supporto -> riconosce le Celle Frame (Ovvero i lati destro e sinistro) della carta schema che non siano anche Celle Corner
+    /**
+     * Metodo supporto: riconosce le Celle Frame (Ovvero i lati destro e sinistro) della carta Side che non siano anche Celle Corner
+     *
+     * @param row riga della cella da classificare
+     * @param col colonna della cella da classificare
+     * @return TRUE se la cella è una Cella Frame, altrimenti FALSE
+     */
+
     private boolean isFrameCell(int row, int col) {
         boolean frame = false;
         if (col == 0 || col == 4) frame = true;
         return frame;
     }
 
-    //Metodo supporto -> A seconda della tipologia di cella (se Corner o Frame), conta la cella stessa in caso di adiecenza
+
+    /**
+     * Metodo supporto che, a seconda della tipologia di cella (se Corner o Frame), conta la cella stessa in caso di adiecenza sulle diagonali
+     * Si noti infatti che i controlli possibili per celle Corner o Celle Frame sono differenti da una cella cosiddetta "normale" presa non ai
+     * bordi
+     *
+     * @param player riferimento alla classe Player (quindi alla Side su cui si sta applicando la carta obbiettivo)
+     * @param row riga della cella da analizzare
+     * @param col colonna della cella da analizzare
+     * @return 1(nel caso di Cella Corner) o al massimo 2 (nel caso di Cella Frame) in caso di adiacenza, altrimenti 0 in entrambi i casi
+     * @throws InvalidColorValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di colore della cella
+     * @throws InvalidShadeValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di sfumatura della cella.
+     * @throws InvalidCoordinatesException lanciata quando le coordinate inserite non sono valide.
+     */
+
     private int hasFrameNeighbors(Player player, int row, int col) throws InvalidColorValueException, InvalidShadeValueException, InvalidCoordinatesException {
 
         Color tempColor1;
@@ -81,9 +106,19 @@ public class ColorDiagonals implements StrategyAlgorithm {
         return neighbors;
     }
 
-    //Metodi di supporto per le consideraioni di contorno finali (ultima riga o ultima adiacenza possibile)
 
-    //Metodo di supporto -> controlla se la Cella corner sia un'ultima adiacenza e ritorna 1 (ovvero conta se stessa)
+    /**
+     * Metodo di supporto che controlla se la Cella Corner sia un'ultima adiacenza e ritorna 1 (ovvero conta se stessa)
+     *
+     * @param player riferimento alla classe Player (quindi alla Side su cui si sta applicando la carta obbiettivo)
+     * @param row riga della cella da analizzare
+     * @param col colonna della cella da analizzare
+     * @return 1(nel caso di Cella Corner) o al massimo 2 (nel caso di Cella Frame) in caso di adiacenza, altrimenti 0 in entrambi i casi
+     * @throws InvalidColorValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di colore della cella
+     * @throws InvalidShadeValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di sfumatura della cella.
+     * @throws InvalidCoordinatesException lanciata quando le coordinate inserite non sono valide.
+     */
+
     private int lastCornerNeighbors(Player player, int row, int col) throws InvalidColorValueException, InvalidShadeValueException, InvalidCoordinatesException {
 
         Dice dieTemp1 = player.showSelectedCell(row,col).showDice();
@@ -114,7 +149,19 @@ public class ColorDiagonals implements StrategyAlgorithm {
         return neighbors;
     }
 
-    //Metodo di supporto -> controlla se la Cella Frame sia un'ultima adiacenza e ritorna 1 (ovvero conta se stessa)
+
+    /**
+     * Metodo di supporto che controlla se la Cella Frame sia un'ultima adiacenza e ritorna 1 (ovvero conta se stessa)
+     *
+     * @param player riferimento alla classe Player (quindi alla Side su cui si sta applicando la carta obbiettivo)
+     * @param row riga della cella da analizzare
+     * @param col colonna della cella da analizzare
+     * @return 1(nel caso di Cella Corner) o al massimo 2 (nel caso di Cella Frame) in caso di adiacenza, altrimenti 0 in entrambi i casi
+     * @throws InvalidColorValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di colore della cella
+     * @throws InvalidShadeValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di sfumatura della cella.
+     * @throws InvalidCoordinatesException lanciata quando le coordinate inserite non sono valide.
+     */
+
     private int lastFrameNeighbors(Player player, int row, int col) throws InvalidColorValueException, InvalidShadeValueException, InvalidCoordinatesException {
 
         Dice dieTemp1 = player.showSelectedCell(row,col).showDice();
@@ -142,12 +189,36 @@ public class ColorDiagonals implements StrategyAlgorithm {
         return neighbors;
     }
 
-    //Metodo di supporto -> ritorna TRUE se la cella non ha prossime adiacenze (controllo che viene fatto per le celle non appartenenti all'ultima riga)
+
+    /**
+     * Metodo di supporto che ritorna TRUE se la cella non ha prossime adiacenze (controllo che viene fatto per le celle non appartenenti all'ultima riga)
+     *
+     * @param player riferimento alla classe Player (quindi alla Side su cui si sta applicando la carta obbiettivo)
+     * @param row riga della cella da analizzare
+     * @param col colonna della cella da analizzare
+     * @return 1(nel caso di Cella Corner) o al massimo 2 (nel caso di Cella Frame) in caso di adiacenza, altrimenti 0 in entrambi i casi
+     * @throws InvalidColorValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di colore della cella
+     * @throws InvalidShadeValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di sfumatura della cella.
+     * @throws InvalidCoordinatesException lanciata quando le coordinate inserite non sono valide.
+     */
+
     private boolean isLastCellNeighbors(Player player, int row, int col) throws InvalidColorValueException, InvalidShadeValueException, InvalidCoordinatesException {
         return hasFrameNeighbors(player,row,col)==0;
     }
 
-    //Metodo supporto -> Conta in aggiunta l'ultima adiacenza in caso di corrispondenza
+
+    /**
+     * Metodo supporto che Conta in aggiunta l'ultima adiacenza in caso di corrispondenza
+     *
+     * @param player riferimento alla classe Player (quindi alla Side su cui si sta applicando la carta obbiettivo)
+     * @param row riga della cella da analizzare
+     * @param col colonna della cella da analizzare
+     * @return 1(nel caso di Cella Corner) o al massimo 2 (nel caso di Cella Frame) in caso di adiacenza, altrimenti 0 in entrambi i casi
+     * @throws InvalidColorValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di colore della cella
+     * @throws InvalidShadeValueException lanciata quando il dado eventuale della cella non rispetta la restrizione di sfumatura della cella.
+     * @throws InvalidCoordinatesException lanciata quando le coordinate inserite non sono valide.
+     */
+
     private int lastNeighbors(Player player, int row, int col) throws InvalidColorValueException, InvalidShadeValueException, InvalidCoordinatesException {
 
         int neighbors = 0;
@@ -162,6 +233,15 @@ public class ColorDiagonals implements StrategyAlgorithm {
         }
         return neighbors;
     }
+
+
+    /**
+     * Metodo che implementa l'algoritmo associato alla carta Obbiettivo
+     *
+     * @param player riferimento alla classe Player (quindi alla Side su cui si sta applicando la carta obbiettivo)
+     * @return i segnalini guadagnati dal Player su cui si è applicata la carta Obbiettivo
+     * @throws Exception viene sollevata in caso di Eccezione generica
+     */
 
     @Override
     public int use(Player player) throws Exception {
