025ab9e75470d7a169cd47e51f9d4b3848615891
Kevin Mato <kevin.mato@mail.polimi.it>
2018-05-28 09:52:16 +0200

    controller aggiornato

--- src/main/java/it/polimi/se2018/server/controller/ControllerTurn.java
+++ src/main/java/it/polimi/se2018/server/controller/ControllerTurn.java
@@ -1,14 +1,17 @@
 package it.polimi.se2018.server.controller;
 
+import it.polimi.se2018.server.exceptions.InvalidHowManyTimes;
 import it.polimi.se2018.server.exceptions.InvalidValueException;
 import it.polimi.se2018.server.model.Player;
 import it.polimi.se2018.server.model.Table;
 
 public class ControllerTurn {
     private Table lobby;
-    private String turnOf;
+    private String turnOf=null;
     private int numbOfPlayers;
     private int round;
+    private int caller=0;
+    private boolean upDown=true;
 
 
     public ControllerTurn(Table LOBBY){
@@ -23,4 +26,13 @@ public class ControllerTurn {
         return round;
     }
 
+    public void setRound() throws InvalidHowManyTimes {
+        turnOf=lobby.callPlayerByNumber(caller).getName();
+        lobby.callPlayerByNumber(caller).reductor();
+
+        if(caller==numbOfPlayers-1) upDown=false;
+        if(caller==0) upDown=true;
+        if(upDown) caller=caller+1;
+        else caller=caller-1;
+    }
 }
