f3b40c6995392ef52b54493e429237ddb56840fc
paoloMRZ <paolo3.marazzi@mail.polimi.it>
2018-05-29 21:56:26 +0200

    Ho aggiunto un'eccezione per notificare quando si ha a che fare con un nickname non valido.

---
+++ src/main/java/it/polimi/se2018/server/exceptions/InvalidNicknameException.java
@@ -0,0 +1,11 @@
+package it.polimi.se2018.server.exceptions;
+/**
+ * La classe rappresenta un'eccezione che viene sollevata quando si ah a che fare con un nickanme non valido.
+ * @author Marazzi Paolo
+ */
+public class InvalidNicknameException extends SagradaException{
+    /**
+     * Costruttore della classe.
+     */
+    public InvalidNicknameException(){super();}
+}
