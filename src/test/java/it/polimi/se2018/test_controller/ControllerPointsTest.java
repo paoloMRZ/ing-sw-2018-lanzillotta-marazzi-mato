f9364ff994902f337c3286528dd03ff075862f71
simolanzillotta <simone.lanzillotta@mail.polimi.it>
2018-05-30 18:00:38 +0200

    Aggiornato caso di test relativo a ControllerPoints

--- src/test/java/it/polimi/se2018/test_controller/ControllerPointsTest.java
+++ src/test/java/it/polimi/se2018/test_controller/ControllerPointsTest.java
@@ -15,7 +15,6 @@ import it.polimi.se2018.server.model.card.card_objective.obj_algos.algos.ShadeVa
 import it.polimi.se2018.server.model.card.card_objective.obj_algos.algos.ShadesOfCard;
 import it.polimi.se2018.server.model.card.card_schema.Cell;
 import it.polimi.se2018.server.model.card.card_schema.Side;
-import it.polimi.se2018.server.model.card.card_utensils.Utensils;
 import it.polimi.se2018.server.model.dice_sachet.Dice;
 import it.polimi.se2018.server.model.dice_sachet.DiceSachet;
 import it.polimi.se2018.server.model.grid.RoundGrid;
@@ -42,7 +41,6 @@ public class ControllerPointsTest {
 
     @Before
     public void setuUpCard() throws InvalidShadeValueException, InvalidFavoursValueException, InvalidShadeException, NoDicesNearException, NotEmptyCellException, InvalidColorException, InvalidCoordinatesException, NearDiceInvalidException {
-
 
         //Creazione carte Side
 
@@ -72,69 +70,6 @@ public class ControllerPointsTest {
         casualListSideOne.add(new Cell(Color.WHITE, 0));
         casualListSideOne.add(new Cell(Color.WHITE, 0));
 
-
-        sides.add(new Side("Aurorae Sagratis",4, casualListSideOne));
-
-        sides.get(0).put(0,0, new Dice(Color.RED, 3));
-        sides.get(0).put(0,1, new Dice(Color.GREEN, 2));
-        sides.get(0).put(0,2, new Dice(Color.BLUE, 6));
-        sides.get(0).put(0,3, new Dice(Color.PURPLE, 2));
-        sides.get(0).put(0,4, new Dice(Color.YELLOW, 3));
-
-        sides.get(0).put(1,0, new Dice(Color.YELLOW, 4));
-        sides.get(0).put(1,1, new Dice(Color.PURPLE, 5));
-        sides.get(0).put(1,2, new Dice(Color.RED, 3));
-        sides.get(0).put(1,3, new Dice(Color.GREEN, 1));
-        sides.get(0).put(1,4, new Dice(Color.PURPLE, 2));
-
-        sides.get(0).put(2,0, new Dice(Color.PURPLE, 3));
-        sides.get(0).put(2,1, new Dice(Color.YELLOW, 1));
-        sides.get(0).put(2,2, new Dice(Color.PURPLE, 2));
-        sides.get(0).put(2,3, new Dice(Color.BLUE, 5));
-        sides.get(0).put(2,4, new Dice(Color.GREEN, 3));
-
-        sides.get(0).put(3,0, new Dice(Color.YELLOW, 5));
-        sides.get(0).put(3,1, new Dice(Color.GREEN, 4));
-        sides.get(0).put(3,2, new Dice(Color.YELLOW, 6));
-
-
-
-        //Settaggio manuale dell carte obbiettivo pubbliche
-        objectives.add(new Objective("TesterName1", "TesterDescription1", 0, new ColorDiagonals(), false));
-        objectives.add(new Objective("TesterName2", "TesterDescription2", 5, new ShadeVariety(), false));
-        objectives.add(new Objective("TesterName3", "TesterDescription3", 2, new CoupleOfShades(1,2), false));
-
-
-        //Settaggio manuale della carta obbiettivo Privata del player 1
-        objectivePrivateOne = new Objective("TesterNamePrivate", "TesterDescriptionPrivate", 0, new ShadesOfCard(Color.YELLOW), true);
-
-        //Settaggio manuale dei player
-        players.add(new Player(objectivePrivateOne, "SIMONE"));
-
-
-        //Settaggio carta Side per i player
-        players.get(0).setSideSelection(sides);
-        players.get(0).setMySide(0);
-        players.get(0).setFavours();
-
-    }
-
-
-
-    @Test
-    public void checkPointObjective() throws Exception {
-
-        lobby = new Table(null, objectives, players, new DiceSachet(), new ScoreGrid(players.size()),new RoundGrid());
-        controllerPoints = new ControllerPoints(lobby);
-
-        controllerPoints.updateScoreOfPlayer();
-        assertEquals( 46,lobby.getScoreGrid().getPlayersPoint(0));
-    }
-
-
-    @Test
-    public void searchWinnerTwoPlayers() throws Exception {
-
         //Kaleidoscopic Dream
 
         casualListSideTwo.add(new Cell(Color.YELLOW , 0));
@@ -161,7 +96,68 @@ public class ControllerPointsTest {
         casualListSideTwo.add(new Cell(Color.BLUE, 0));
         casualListSideTwo.add(new Cell(Color.YELLOW, 0));
 
+
+        sides.add(new Side("Aurorae Sagratis",4, casualListSideOne));
         sides.add(new Side("Kaleidoscopic Dream",4, casualListSideTwo));
+
+        //Settaggio manuale dell carte obbiettivo pubbliche
+        objectives.add(new Objective("TesterName1", "TesterDescription1", 0, new ColorDiagonals(), false));
+        objectives.add(new Objective("TesterName2", "TesterDescription2", 5, new ShadeVariety(), false));
+        objectives.add(new Objective("TesterName3", "TesterDescription3", 2, new CoupleOfShades(1,2), false));
+
+
+    }
+
+
+
+    @Test
+    public void checkPointObjective() throws Exception {
+
+        sides.get(0).put(0,0, new Dice(Color.RED, 3));
+        sides.get(0).put(0,1, new Dice(Color.GREEN, 2));
+        sides.get(0).put(0,2, new Dice(Color.BLUE, 6));
+        sides.get(0).put(0,3, new Dice(Color.PURPLE, 2));
+        sides.get(0).put(0,4, new Dice(Color.YELLOW, 3));
+
+        sides.get(0).put(1,0, new Dice(Color.YELLOW, 4));
+        sides.get(0).put(1,1, new Dice(Color.PURPLE, 5));
+        sides.get(0).put(1,2, new Dice(Color.RED, 3));
+        sides.get(0).put(1,3, new Dice(Color.GREEN, 1));
+        sides.get(0).put(1,4, new Dice(Color.PURPLE, 2));
+
+        sides.get(0).put(2,0, new Dice(Color.PURPLE, 3));
+        sides.get(0).put(2,1, new Dice(Color.YELLOW, 1));
+        sides.get(0).put(2,2, new Dice(Color.PURPLE, 2));
+        sides.get(0).put(2,3, new Dice(Color.BLUE, 5));
+        sides.get(0).put(2,4, new Dice(Color.GREEN, 3));
+
+        sides.get(0).put(3,0, new Dice(Color.YELLOW, 5));
+        sides.get(0).put(3,1, new Dice(Color.GREEN, 4));
+        sides.get(0).put(3,2, new Dice(Color.YELLOW, 6));
+
+        //Settaggio manuale della carta obbiettivo Privata del player 1
+        objectivePrivateOne = new Objective("TesterNamePrivate", "TesterDescriptionPrivate", 0, new ShadesOfCard(Color.YELLOW), true);
+
+        //Settaggio manuale dei player
+        players.add(new Player(objectivePrivateOne, "SIMONE"));
+
+
+        //Settaggio carta Side per i player
+        players.get(0).setSideSelection(sides);
+        players.get(0).setMySide(0);
+        players.get(0).setFavours();
+
+        lobby = new Table(null, objectives, players, new DiceSachet(), new ScoreGrid(players.size()),new RoundGrid());
+        controllerPoints = new ControllerPoints(lobby);
+
+        controllerPoints.updateScoreOfPlayer();
+
+        assertEquals( 46,lobby.getScoreGrid().getPlayersPoint(0));
+    }
+
+
+    @Test
+    public void searchWinnerTwoPlayers() throws Exception {
 
         sides.get(1).put(0,0, new Dice(Color.YELLOW, 6));
         sides.get(1).put(0,1, new Dice(Color.BLUE, 2));
