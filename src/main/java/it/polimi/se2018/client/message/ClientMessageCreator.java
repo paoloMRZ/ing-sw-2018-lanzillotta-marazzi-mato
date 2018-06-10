8f1ac67ff27fd417c75792670cbb17ab6d05589e
paoloMRZ <paolo3.marazzi@mail.polimi.it>
2018-06-10 21:16:27 +0200

    Classe per la creazione dei messaggi che il client invia al server tramite la rete.

---
+++ src/main/java/it/polimi/se2018/client/message/ClientMessageCreator.java
@@ -0,0 +1,67 @@
+package it.polimi.se2018.client.message;
+
+import java.util.List;
+
+public class ClientMessageCreator {
+
+    private ClientMessageCreator(){}
+
+
+    /**
+     * Il metodo costruisce un messaggio per notificare al server la propria disconnessione.
+     *
+     * @param sender nickname del client che invia il messaggio.
+     * @return messaggio di disconnessione.
+     */
+
+    public static String getDisconnectMessage(String sender){
+        return "/" + sender + "/###/network/disconnected/?\n";
+    }
+
+    /**
+     * Il metodo costruisce un messaggio per notificare al server l'intenzione di eseguire un piazzamento sulla
+     * prorpria griglia di gioco.
+     *
+     * @param sender nickname del client che invia il messaggio.
+     * @param dieIndex indice del dado scelto dalla riserva.
+     * @param row riga in cui piazzare il dado.
+     * @param col colonna in cui piazzare il dado.
+     * @return messaggio di piazzamento.
+     */
+
+    public static String getPutDiceMessage(String sender, String dieIndex, String row, String col){
+        return "/" + sender + "/###/put/?/" + dieIndex + "&" + row + "&" + col + "\n";
+    }
+
+
+    public static String getActivateUtensilMessage(String sender, String cardIndex){
+        return "/" + sender + "/###/utensil/activate/" + cardIndex + "\n";
+    }
+
+    public static String getActivateUtensilMessage(String sender, String cardIndex, String param1){
+        return "/" + sender + "/###/utensil/activate/" + cardIndex + "&" + param1 + "\n";
+    }
+
+    public static String getActivateUtensilMessage(String sender, String cardIndex, String param1, String param2){
+        return "/" + sender + "/###/utensil/activate/" + cardIndex + "&" + param1 + "&" + param2 + "\n";
+    }
+
+    public static String getSideReplyMessage(String addressee, String cardName){
+        return "/" + addressee + "/###/start/side_reply/" + cardName + "\n";
+    }
+
+    public static String getPassTurnMessage(String sender){
+        return "/" + sender + "/###/update/turn/?\n";
+    }
+
+    public static String getUseUtensilMessage(String sender, String cardIndex ,List<String> paramList){
+        String message = "/" + sender + "/###/utensil/use/" + cardIndex;
+
+        for(String param: paramList)
+            message = message.concat("&" + param);
+
+        return message.concat("\n");
+    }
+
+
+}
