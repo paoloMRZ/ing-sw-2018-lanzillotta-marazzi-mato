31bc0b222bd1c11672f0f377e6220e6a34200221
Kevin Mato <kevin.mato@mail.polimi.it>
2018-06-12 11:02:53 +0200

    Aggioranmento messaggistica

---
+++ src/main/java/it/polimi/se2018/server/events/responses/ChangingTurn.java
@@ -0,0 +1,7 @@
+package it.polimi.se2018.server.events.responses;
+
+public class ChangingTurn extends TimeIsUp {
+    public ChangingTurn(String player){
+        super(player);
+    }
+}
--- src/main/java/it/polimi/se2018/server/events/responses/ErrorActivation.java
+++ src/main/java/it/polimi/se2018/server/events/responses/ErrorActivation.java
@@ -3,7 +3,7 @@ package it.polimi.se2018.server.events.responses;
 public class ErrorActivation extends SuccessActivation {
 
     public ErrorActivation(int index, String message, String player){
-        super(index,message,player);
-    }
+
+        super(message,index, 0, 0,player);    }
 
 }
